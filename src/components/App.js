0877de0d3691822e123084f5f75ad5224eca39d7
Philip Rolink <philiprolink@gmail.com>
2018-11-12 21:56:19 +0000

    More work completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import '../scss/App.scss';
 import Nav from './Nav'
+import Banner from './Banner'
 
 class App extends Component {
   render() {
     return (
-      <Nav />
+      <div>
+        <Nav />
+        <Banner />
+      </div>
     );
   }
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../scss/Banner.scss';
+
+class Banner extends Component {
+  render() {
+    return (
+        <div className="container pr-banner">
+            <h1>Philip<br />Rolink</h1>
+            <p>Front End Developer</p>
+        </div>
+    );
+  }
+}
+
+export default Banner;
